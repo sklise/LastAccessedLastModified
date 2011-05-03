3abcb75a824aef207ee50b12a970d308ec015938
Steven Klise <steven@stevenklise.com>
2011-05-03 00:57:30 -0400

    More weather, some file permissions

--- observations.py
+++ observations.py
@@ -2,18 +2,18 @@
 
 import urllib
 from xml.dom import minidom
+import os
+import stat
+
 
 ### Observations
 zipcode = "10003"
 
-### Weather data
-wurl = "http://xml.weather.yahoo.com/forecastrss?p="+zipcode+"&u=f";
-r = minidom.parse(urllib.urlopen(wurl)) # open each url as a dom element
-lat = r.getElementsByTagName('geo:lat')[0].firstChild.nodeValue
-lon = r.getElementsByTagName('geo:long')[0].firstChild.nodeValue
-conditions = r.getElementsByTagName('yweather:condition')[0]
-
 ### File Info
+# http://leiqing.org/mirrors/python-2.7.1-docs-html/library/stat.html
+def isgroupreadable(filepath):
+  st = os.stat(filepath)
+  return bool(st.st_mode & stat.S_IWGRP)
 
 ### Context Free Grammars => For when we aren't anywhere
 
@@ -23,25 +23,40 @@ zips = open('shortzips.txt')
 for line in zips:
 	line = line.strip()
 	bits = line.split(",")
-	# print bits[1].strip('"')
 	zipcodes[int(bits[1].strip('"'))] = bits[3].strip('"')
 
+### Obseerve class:: Where the poem is written.
 class Observe(object):
 	
 	def __init__(self):
 		self.output = ""
 	
-	def wearehere(self,loc):
+	def weather(self,loc):
+		### Weather data
+		wurl = "http://xml.weather.yahoo.com/forecastrss?p="+str(loc)+"&u=f";
+		r = minidom.parse(urllib.urlopen(wurl)) # open each url as a dom element
+		# lat = r.getElementsByTagName('geo:lat')[0].firstChild.nodeValue
+		# lon = r.getElementsByTagName('geo:long')[0].firstChild.nodeValue
+		self.condition = r.getElementsByTagName('yweather:condition')[0].getAttribute("text")
+		self.sunrise = r.getElementsByTagName('yweather:astronomy')[0].getAttribute("sunrise")
+	
+	def whichcity(self,loc):
 		import random
 		
 		if loc != int(zipcode):
 			self.output += "This is not "+zipcodes[int(loc)]+"\n"
-			self.wearehere(random.choice(zipcodes.keys()))
+			try: 
+				self.weather(loc)
+				self.output += "    It would be "+self.condition+"\n"
+				self.output += "    Sunrise "+self.sunrise+"\n"
+			except:
+				self.output +="\n"
+			self.whichcity(random.choice(zipcodes.keys()))
 		else:
 			self.output += "We are in "+zipcodes[int(zipcode)]+"\n"
 
-
 if __name__ == '__main__':
+	print isgroupreadable("/Users/sklise/ITP/S11_RWET/Observations")
 	poem = Observe()
-	poem.wearehere(11222)
+	poem.whichcity(11222)
 	print poem.output
