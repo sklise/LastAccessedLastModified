c85a422d70a64e76aac623cff365561d07641ba5
Steven Klise <steven@stevenklise.com>
2011-05-04 09:44:48 -0400

    Ignore script generated txt files.

--- selfdiscovery.py
+++
@@ -1,9 +0,0 @@
-class SelfDiscovery(object):
-	
-	def __init__(self):
-		self.ip = tuple() # [X,X,X,X]
-		self.inittime = dict() # {year,month,date,hour,minute}
-		self.lastmod = dict() # {year,month,date,hour,minute}
-		self.permissions = dict() # {user,group,other}
-		self.git = dict() # {commit,count}
-		
